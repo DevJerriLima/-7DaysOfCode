99327d679d29db618521ac358e3d16fb9e9d31cf
Jerri Lima <dev.jerrilima@gmail.com>
2023-01-26 22:15:23 -0400

    Segundo e tercerio ifs resolvidos

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ let numeroTrinta = 30
 let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
-/*O comparador == imprime no console a primeira linha, já que os valores são iguais apesar de serem de tipos diferentes, o interpretador ignora o tipo da variavel
-O comparador === imprime a a segunda linha ja que leva em comparação para se ter o valor true tanto o conteúdo da variavel quanto o tipo
-*/
+
 if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
 
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
 
