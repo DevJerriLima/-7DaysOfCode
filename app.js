e7af8202b8a30ea6bfd9aa5c1ec81b43861a4dcd
Jerri Lima <dev.jerrilima@gmail.com>
2023-01-26 22:05:34 -0400

    primeiro if resolvido

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ let numeroTrinta = 30
 let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
-
+/*O comparador == imprime no console a primeira linha, já que os valores são iguais apesar de serem de tipos diferentes, o interpretador ignora o tipo da variavel
+O comparador === imprime a a segunda linha ja que leva em comparação para se ter o valor true tanto o conteúdo da variavel quanto o tipo
+*/
 if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
+
+
 
